9e10c9ae7675cb301d69f83c26a4903f09d3ad05
Helmi Pradita <helmipraditaa@gmail.com>
2022-09-23 23:34:21 +0700

    finish task1.js

---
+++ task1.js
@@ -0,0 +1,9 @@
+var name = 'Helmi Pradita';
+var email = 'helmipraditaa@gmail.com';
+var address = {
+    jalan: 'Anggrek No 2 RT 22 RW 07 Tegalsari',
+    kodepos: '61364',
+    kota: 'Mojokerto',
+};
+var isMerried = false;
+var hobbies = ["Membaca", "Menonton", "Sepak Bola"];
