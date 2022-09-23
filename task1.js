3ce9f23d48a309682f01a106a492b6fd9127d099
Helmi Pradita <helmipraditaa@gmail.com>
2022-09-23 23:35:34 +0700

    finish update task1.js

--- task1.js
+++ task1.js
@@ -1,9 +1,19 @@
-var name = 'Helmi Pradita';
-var email = 'helmipraditaa@gmail.com';
-var address = {
-    jalan: 'Anggrek No 2 RT 22 RW 07 Tegalsari',
-    kodepos: '61364',
-    kota: 'Mojokerto',
-};
-var isMerried = false;
-var hobbies = ["Membaca", "Menonton", "Sepak Bola"];
+function biodata () {
+    var name = 'Helmi Pradita';
+    var email = 'helmipraditaa@gmail.com';
+    var address = {
+        jalan: 'Anggrek No 2 RT 22 RW 07 Tegalsari',
+        kodepos: '61364',
+        kota: 'Mojokerto',
+    };
+    var isMerried = false;
+    var hobbies = ["Membaca", "Menonton", "Sepak Bola"];
+
+    console.log(name);
+    console.log(email);
+    console.log(address);
+    console.log(isMerried);
+    console.log(hobbies);
+}
+
+biodata();
